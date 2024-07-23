ac369daf9c16a3f9a2063a459e1e7150564cf279
邸俊杰 <dijunjie@noahgroup.com>
2024-07-23 22:53:26 +0800

    fix: bug

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,8 @@
-module.exports = ({ env }) => [
+module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  // 添加 CORS 配置对象
-  {
-    name: "cors",
-    config: {
-      enabled: true,
-      origin: [
-        "https://radiant-frogs-686c42c943.strapiapp.com",
-        "https://h5-t3.noahgrouptest.com",
-      ], // 允许访问的域名
-      credentials: true, // 允许携带 cookies
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
